f826f85ca2532ff6d9a3a9ea6bb54581a7e9e21f
Pavlo Sheremet <pashqa.sheremet@gmail.com>
2025-04-22 21:38:51 +0300

    save 1,3 task

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,29 @@ import Heading from './components/Heading/Heading';
 import Section from './components/Section/Section';
 import Statistics from './components/Statistics/Statistics';
 
+import articeData from './data/article.json';
+import forbesData from './data/forbes.json';
+
 export const App = () => {
   return (
     <Section>
       <Container>
         <Heading title="Task 1 Blog Card" bottom />
-        <BlogCard />
+        <BlogCard
+          avatar={articeData.avatar}
+          description={articeData.description}
+          name={articeData.name}
+          postedAt={articeData.postedAt}
+          poster={articeData.poster}
+          tag={articeData.tag}
+          title={articeData.title}
+        />
 
         <Heading title="Task 2 Statistics" top bottom />
         <Statistics title="Main Statistics" />
 
         <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList list={forbesData} />
 
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
